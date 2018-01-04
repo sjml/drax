b6731a57f169ddca60a1dd4eb7a2e7ce73051ee6
Shane Liesegang <shane@techie.net>
2018-01-04 09:56:45 -0500

    apply my patch until the CodeMirror PR is approved

--- shipitfile.js
+++ shipitfile.js
@@ -24,8 +24,13 @@ module.exports = function(shipit) {
 
   shipit.blTask('build', function () {
     shipit.log(chalk.green('Making fresh production build'));
+    const commands = [
+      'npm install',
+      'curl https://patch-diff.githubusercontent.com/raw/codemirror/CodeMirror/pull/5156.diff | patch -d node_modules/codemirror -p1',
+      'npm run build'
+    ];
     return shipit.local(
-            'npm install; npm run build',
+            commands.join('; '),
             {cwd: shipit.config.workspace}
           )
           .then(function () {
