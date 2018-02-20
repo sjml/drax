8bc41ce356843d11ad7c158d161935473d01504b
Shane Liesegang <shane@techie.net>
2018-02-19 21:00:08 -0500

    internationalizing the sample names

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -8,6 +8,21 @@ import * as CodeMirror from 'codemirror';
 import * as fns from 'date-fns';
 
 const pgmd = require('!raw-loader!./playground.md');
+const names = {
+  F : [
+    'Sofia', 'Valentina', 'Regina', 'Ximena', 'Alejandra', 'Emma', 'Emily',
+    'Ava', 'Charlotte', 'Amelia', 'Iminathi', 'Minenhle', 'Rethabile',
+    'Bokamoso', 'Karabo', 'Yu-Tung', 'Pin-Yen', 'Yung-Ching', 'Tzu-Ching',
+    'Pin-Yu', 'Margret', 'Anna', 'Hekla', 'Isabella', 'Eva'
+  ],
+  M : [
+    'Santiago', 'Mateo', 'Leonardo', 'Emiliano', 'Diego', 'Oliver', 'Liam',
+    'Lucas', 'Jacob', 'William', 'Bandile', 'Kungawo', 'Siyabonga', 'Lethabo',
+    'Khayone', 'Yu-Hsiang', 'Yu-Ting', 'Yu-En', 'Cheng-En', 'Yu-Chen', 'Aron',
+    'Alexander', 'Viktor', 'Kristjan', 'Jon'
+  ]
+};
+
 
 interface SampleAnnotation {
   name: string;
@@ -42,30 +57,44 @@ export class PlaygroundComponent implements AfterViewInit {
     const doc = this.editor.instance.getDoc();
     doc.clearHistory();
 
+    // bad performance, but N is small
+    function shuffle(a) {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    }
+    shuffle(names.F);
+    shuffle(names.M);
+
+    const fNames = names.F.slice(0, 2);
+    const mNames = names.M.slice(0, 2);
+
     const sampleAnnotations: SampleAnnotation[] = [
       {
-        name: 'Alice',
+        name: fNames[0],
         from: {line: 18, ch: 62},
         to:   {line: 18, ch: 67},
         text: 'I always preferred the UK spelling. Can we change this to "colour?"',
         stamp: fns.subDays(Date.now(), 2).getTime()
       },
       {
-        name: 'Bob',
+        name: mNames[0],
         from: {line: 18, ch: 107},
         to:   {line: 18, ch: 120},
         text: 'This shouldn\'t be italicized, if I remember my typographical rules.',
         stamp: fns.subDays(Date.now(), 4).getTime()
       },
       {
-        name: 'Richard',
+        name: mNames[1],
         from: {line: 18, ch: 155},
         to:   {line: 18, ch: 170},
         text: 'This is confusing. Either call them annotations or call them comments, but just pick one!',
         stamp: fns.subDays(Date.now(), 1).getTime()
       },
       {
-        name: 'Margaret',
+        name: fNames[1],
         from: {line: 18, ch: 186},
         to:   {line: 18, ch: 195},
         text: 'It might be best to avoid politics if we can.',
